fe6d19e0a0bb01be9b0856494e56d4062bacf33e
Rachid Daniel Tekour <75126710+Tekour18CZ@users.noreply.github.com>
2020-11-30 22:04:06 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,78 @@
+print('Snaž se odpovídat jednoznačně, pokud si nebudeš jistý, zvol odpověď IDK (Nevím) a dostaneš případně jinou radu.')
+
+choice = input("Hledáš nový vztah ? Odpověz: Ano / Ne / Nevím")
+
+if choice == "Ano":
+
+    print("Choď mezi lidi a uvolni se, věř si, tím bude vyšší šance na vztah")
+
+if choice == "Ne":
+
+    print("Nevadí, jednou si určitě někoho najdeš")
+
+if choice == "Nevím":
+
+    print("V tom případě není kam spěchat, čas přijde")
+
+else: "Nevhodný formát odpovědi, zkus to znovu"
+
+
+choice = input("Jsi oblíbený u holek ? Odpověz: Ano / Ne / Nevím")
+
+if choice == "Ano":
+
+    print("Jistě pro tebe není problém s navázáním kontaktu, stačí jen iniciativa od tebe.")
+
+if choice == "Ne":
+
+    print("Je potřeba zapracovat na své osobnosti, nabrat sebevědomí a více komunikovat.")
+
+if choice == "Nevím":
+
+    print("Stačí se nebát a ujistit se.")
+
+else: "Nevhodný formát odpovědi, zkus to znovu"
+
+choice = input("Myslíš si, že máš dostatek sebevědomí ? Odpověz: Ano / Ne / Nevím")
+
+if choice == "Ano":
+
+    print("Je dobře, že máš dost sebedůvěry, ale nesmí ničeho být moc.")
+
+if choice == "Ne":
+
+    print("Najdi si vzor a víc si věř.")
+
+if choice == "Nevím":
+
+    print("Komunikuj s lidmi a dej si velké cíle, uvidíš, jak se budeš cítit.")
+
+else: "Nevhodný formát odpovědi, zkus to znovu"
+
+choice = input("Našel sis tajnou lásku ? Odpověz: Ano / Ne / Nevím")
+
+if choice == "Ano":
+
+    print("Neboj se jí sám sebe ukázat a dát jí najevo, že po ní toužíš")
+
+if choice == "Ne":
+
+    print("Nevadí, nic nespěchá.")
+
+else: "Nevhodný formát odpovědi, zkus to znovu"
+
+choice = input("Máš dostatek kamarádů? Odpověz: Ano / Ne / Nevím")
+
+if choice == "Ano":
+
+    print("Pak není co řešit, o kontakty máš postaráno.")
+
+if choice == "Ne":
+
+    print("Možná jen málo trávíš čas venku, zkus se víc zapojit k lidem v konverzaci.")
+
+if choice == "Nevím":
+
+    print("Pokud cítíš, že spíš ne, tak se spřátel s více lidmi, jinak si udržuj aktuální přátele, bude se to hodit.")
+
+else: "Nevhodný formát odpovědi, zkus to znovu"
